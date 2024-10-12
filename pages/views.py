31bf7ef7c2898d0dc4812b50762afb7cfebda7cf
Laureano <laureanosciacaluga@gmail.com>
2024-10-12 09:15:22 -0300

    Personalizar formularios

--- pages/views.py
+++ pages/views.py
@@ -1,40 +1,29 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Page
 from django.urls import reverse, reverse_lazy
+from .forms import PageForm
 
 # Create your views here.
 class PageListView(ListView):
     model = Page
 
-# def pages(request):
-#     pages = get_list_or_404(Page)
-#     return render(request, 'pages/pages.html', {'pages':pages})
-
 class PageDetailView(DetailView):
     model = Page
     
-# def page(request, page_id, page_slug):
-#     page = get_object_or_404(Page, id=page_id)
-#     return render(request, 'pages/page.html', {'page':page})
-
-
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages')
-    
     
 class PageUpdateView(UpdateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     template_name_suffix = "_update_form"
     
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
-    
     
 class PageDeleteView(DeleteView):
     model = Page
