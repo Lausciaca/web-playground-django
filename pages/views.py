33b07716125a811de920040d3009f3429cc0765a
Laureano <laureanosciacaluga@gmail.com>
2024-10-12 08:20:36 -0300

    CreateView

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Page
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 class PageListView(ListView):
@@ -17,3 +19,9 @@ class PageDetailView(DetailView):
 # def page(request, page_id, page_slug):
 #     page = get_object_or_404(Page, id=page_id)
 #     return render(request, 'pages/page.html', {'page':page})
+
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
