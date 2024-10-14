19743656238577e5b6a1d8191a2c13319d4321a7
Laureano <laureanosciacaluga@gmail.com>
2024-10-14 07:24:03 -0300

    Añadir verificacion para usuario, mixins

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,19 @@
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Page
+from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
 from .forms import PageForm
+
+# staff required mixin
+class StaffRequiredMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super().dispatch(request, *args, **kwargs)
 
 # Create your views here.
 class PageListView(ListView):
@@ -12,12 +22,12 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
     
-class PageCreateView(CreateView):
+class PageCreateView(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
     
-class PageUpdateView(UpdateView):
+class PageUpdateView(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
@@ -25,6 +35,6 @@ class PageUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
     
-class PageDeleteView(DeleteView):
+class PageDeleteView(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy("pages:pages")
